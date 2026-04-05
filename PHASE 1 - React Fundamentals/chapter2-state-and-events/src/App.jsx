2036ea4e67335834a067d55ffdabddb54bb53099
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-04-05 18:23:30 +0530

    2. useState Hook Deep Dive

--- PHASE 1 - React Fundamentals/chapter2-state-and-events/src/App.jsx
+++ PHASE 1 - React Fundamentals/chapter2-state-and-events/src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
   return (
     <div className="bg-gray-950">
-      <h1></h1>
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-center">
+        React State & Events
+      </h1>
+
+      {/* 2. useState Hook Deep Dive */}
+      <h2 className="text-xl sm:text-2xl text-gray-50 font-semibold mt-6 mb-3">
+        2. useState Hook Deep Dive
+      </h2>
     </div>
   )
 }
