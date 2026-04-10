8f4211871d9ba1586d560c84f284b0ce9bcf480e
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-04-10 09:43:17 +0530

    Cleanup Function for Timers and Intervals

---
+++ PHASE 1 - React Fundamentals/chapter3-side-effects/src/Side Effects/04-cleanup-functions/01-cleanup-scenarios.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+// Common Cleanup Scenarios
+
+import { useEffect, useState } from "react";
+
+// 1. Timers and Intervals
+
+  function Timer() {
+    const [second, setSeconds] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setSeconds(s => s + 1);
+      }, 1000);
+
+      // Cleanup: clear interval when component unmounts
+      return () => {
+        clearInterval(interval);
+      }
+    }, []);
+
+    return <div>Seconds: {second}</div>
+  }
